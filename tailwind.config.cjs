9fdc0385b2b1f8bea11897a4dbe21ba49951a33e
GoldToothRichards <crabtr26@proton.me>
2024-10-08 21:52:06 -0400

    feat: Add resume

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,14 @@ module.exports = {
   content: ["./src/**/*.astro"],
   darkMode: "class",
   theme: {
+    screens: {
+      'xs': '480px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       fontFamily: {
         body: ["Roboto Mono", "sans-serif"],
