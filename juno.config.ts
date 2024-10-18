d40cf2da8a3b81277f10b462f0481ad876c2d97a
GoldToothRichards <crabtr26@proton.me>
2024-10-18 13:44:06 -0400

    feat: Add Cache-Control headers

--- juno.config.ts
+++ juno.config.ts
@@ -4,5 +4,33 @@ export default defineConfig({
   satellite: {
     id: "ojmfj-uyaaa-aaaal-ajcma-cai",
     source: "dist",
+    storage: {
+      headers: [
+        {
+          source: "/",
+          headers: [["Cache-Control", "public,max-age=0,must-revalidate"]]
+        },
+        {
+          source: "fonts/**/*",
+          headers: [["Cache-Control", "max-age=31536000"]]
+        },
+        {
+          source: "fontawesome/**/*",
+          headers: [["Cache-Control", "max-age=31536000"]]
+        },
+        {
+          source: "**/*.{jpg,jpeg,png,gif,webp,svg,ico}",
+          headers: [["Cache-Control", "max-age=31536000"]]
+        },
+        {
+          source: "**/*.{woff,woff2,ttf,otf}",
+          headers: [["Cache-Control", "max-age=31536000"]]
+        },
+        {
+          source: "documents/**/*",
+          headers: [["Cache-Control", "max-age=0,must-revalidate"]]
+        }
+      ]
+    }
   },
 });
